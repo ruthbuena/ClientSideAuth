78544e2e51273a3ea5cb60d393262a93dfc5017f
Ruth Buenaflor <ruthbpeace@gmail.com>
2017-10-19 15:27:14 -0400

    import browserHistory

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
 
 const ROOT_URL = 'http://localhost:3090';
 
@@ -8,14 +9,14 @@ export function signinUser ({ email, password }) {
     // dispatch({ type: ... })-can essentially pass any logic in here; no asych with thunk
     //Submit email/pw to the server/POST request using Axios-third party library
     // shown in same way below in action creator {email: email, password: password }
-    axios.post(`${ROOT_URL}/signin`, { email, password });
-
-    //If request is good then update state to indicate user has been auth'd
-    //Save JWT token so that users can make follow up requests
-    //Redirect to the route '/feature' - this is for users that are now logged in
-
-    //If request is bad then show an error to users
-
+    axios.post(`${ROOT_URL}/signin`, { email, password })
+      .then(response => {
+        //If request is good then update state to indicate user has been auth'd
+        //Save JWT token so that users can make follow up requests
+        //Redirect to the route '/feature' - this is for users that are now logged in; programmatic navigation
+      })
+      .catch(() => {
+      //If request is bad then show an error to users
+      });
   }
-
 }
