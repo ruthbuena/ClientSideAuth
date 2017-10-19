19998b1bd0a9bac06e7428d0c7aa41dc4f6e5eb8
Ruth Buenaflor <ruthbpeace@gmail.com>
2017-10-19 16:05:01 -0400

    add auth error to switch case in auth reducer

--- src/reducers/auth_reducer.js
+++ src/reducers/auth_reducer.js
@@ -1,16 +1,19 @@
 //whether user is authenticated
 import {
   AUTH_USER,
-  UNAUTH_USER
+  UNAUTH_USER,
+  AUTH_ERROR
 } from '../actions/types';
 
-export default function (state={}, action) {
+export default function (state = {}, action) {
   switch(action.type) {
 //Boolean to show if user has been authenticated
     case AUTH_USER:
       return { ...state, authenticated: true };
     case UNAUTH_USER:
       return { ...state, authenticated: false };
+    case AUTH_ERROR:
+      return { ...state, error: action.payload };
   }
 
   return state;
