fdfd0b4dab5f23769b1cfda5075ca02ea1c936c1
Ruth Buenaflor <ruthbpeace@gmail.com>
2017-10-19 14:27:32 -0400

    pass dispatch

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,19 @@
 
 
 export function signinUser ({ email, password }) {
-  //Submit email/pw to the server
+//return function because this is how we access the dispatch (makes sure action gets sent to reducers)
+  return function(dispatch) {
+    // dispatch({ type: ... })-can essentially pass any logic in here
 
-  //If request is good then update state to indicate user has been auth'd
-  //Save JWT token so that users can make follow up requests
-  //Redirect to the route '/feature' - this is for users that are now logged in
 
-  //If request is bad then show an error to users
-  
+    //Submit email/pw to the server
+
+    //If request is good then update state to indicate user has been auth'd
+    //Save JWT token so that users can make follow up requests
+    //Redirect to the route '/feature' - this is for users that are now logged in
+
+    //If request is bad then show an error to users
+
+  }
+
 }
